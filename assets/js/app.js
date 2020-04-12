152f8cd73fa64d7791120eb776670ce59ec0f04e
ifancyabroad <edgar.nightingale88@gmail.com>
2020-04-12 13:01:22 +0100

    Add circular logo to the header

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+// Rotate the circular logos on window scroll
+$(function() {
+  let rotation = 0, 
+      scrollLoc = $(document).scrollTop();
+  $(window).scroll(function() {
+      const newLoc = $(document).scrollTop();
+      const diff = scrollLoc - newLoc;
+      rotation += diff, scrollLoc = newLoc;
+      const rotationStr = "rotate(" + rotation + "deg)";
+      $("#logo-header").css({
+          "-webkit-transform": rotationStr,
+          "-moz-transform": rotationStr,
+          "transform": rotationStr
+      });
+  });
+});
