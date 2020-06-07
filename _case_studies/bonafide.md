783d2f1fa3dd77c29688e731830127d4e7146209
ifancyabroad <edgar.nightingale88@gmail.com>
2020-06-07 15:16:42 +0100

    Add carousel to case study page

--- _case_studies/bonafide.md
+++ _case_studies/bonafide.md
@@ -1,8 +1,17 @@
 ---
 short_name: bonafide
 name: Bonafide hr
+tag: Creating a modern and striking human resources brand and digital platform from conception to open business.
+title: Building a travel startup from the ground up to Virgin funding
+client: Bonafide hr
+type: Website, Web App
 image: case_study_image.jpg
 alt: Some alt text
 featured: true
+quote: “The way Internet advertising has behaved lately,<br> it may well take 50 years to get there.”
+quote_label: Scott Calver - CEO, Where 2 Go When
+content_title: Stepping up the e-commerce design of a small to medium company.
 ---
-Creating a modern and striking human resources brand and digital platform from conception to open business.
+<p class="mb-4">Native Shoes has an incredible product and loyal brand advocates, but needed help scaling their online business to keep up with customer demands. We helped their team better understand their product and market.</p>
+<p class="mb-4">Native Shoes has an incredible product and loyal brand advocates, but needed help scaling their online business to keep up with customer demands. We helped their team better understand their product and market.</p>
+<p>The result: a newly redesigned, responsive website that contributed to a sales increase of over 30% in the first quarter after launch.</p>
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,46 @@
-// Rotate the circular logos on window scroll
-$(function() {
-  let rotation = 0, 
-      scrollLoc = $(document).scrollTop();
-  $(window).scroll(function() {
-      const newLoc = $(document).scrollTop();
-      const diff = scrollLoc - newLoc;
-      rotation += diff * 0.5, scrollLoc = newLoc;
-      const rotationStr = "rotate(" + rotation + "deg)";
-      $(".scroll-rotate").css({
-          "-webkit-transform": rotationStr,
-          "-moz-transform": rotationStr,
-          "transform": rotationStr
-      });
-  });
-});
+$( document ).ready(function() {
 
-// Fade scroll to anchor links
-$("a.transition").click(function(event) {
+  // Rotate the circular logos on window scroll
+  $(function() {
+    let rotation = 0, 
+        scrollLoc = $(document).scrollTop();
+    $(window).scroll(function() {
+        const newLoc = $(document).scrollTop();
+        const diff = scrollLoc - newLoc;
+        rotation += diff * 0.5, scrollLoc = newLoc;
+        const rotationStr = "rotate(" + rotation + "deg)";
+        $(".scroll-rotate").css({
+            "-webkit-transform": rotationStr,
+            "-moz-transform": rotationStr,
+            "transform": rotationStr
+        });
+    });
+  });
+  
+  // Fade scroll to anchor links
+  $("a.transition").click(function(event) {
     event.preventDefault();
     $("body").fadeOut(500, redirectPage.bind(this, this.href));
   });
-  
+    
   function redirectPage(link) {
     $("body").fadeIn(500);
     setTimeout(function () {
       window.location = link;
     }, 0);
   }
+  
+  // Case study carousel
+  var $carousel = $('.carousel').flickity();
+  
+  // previous
+  $('.previous').on( 'click', function() {
+    console.log($carousel)
+    $carousel.flickity('previous');
+  });
+  
+  // next
+  $('.next').on( 'click', function() {
+    $carousel.flickity('next');
+  });
+});
